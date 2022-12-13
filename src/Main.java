b3e88e5da8bf64b417bafb604cc44c4a59f18572
Alexandra <alexandravolchihina@yandex.ru>
2022-12-13 19:03:41 +0300

    Домашнее задание 5 сделано.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args)
     {
         lessonwork1();
+
     }
         public static void lessonwork1() {
         System.out.println("Самостоятельная задача 1");
